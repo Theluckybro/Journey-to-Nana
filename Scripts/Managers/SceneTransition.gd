d64419726a4d457102289ddfa14d6d6772389a6e
Theluckybro <indrawahyu856@gmail.com>
2025-11-17 09:33:36 +0700

    added Transition Screen

--- Scripts/Managers/SceneTransition.gd
+++ Scripts/Managers/SceneTransition.gd
@@ -1,4 +1,6 @@
-extends Node
+extends CanvasLayer
+
+@onready var animation_player: AnimationPlayer = $TransitionAnimation
 
 var player: PlayerMain
 var last_scene_name: String
@@ -21,9 +23,20 @@ func change_scene(player_node: Node, destination_scene: String, _facing: Vector2
 	player = player_node as PlayerMain
 
 	var parent = player.get_parent()
+
+	if player:
+		player.can_move = false
+
+	animation_player.play("transition_out")
+	await animation_player.animation_finished
+
 	if parent:
 		parent.remove_child(player)
 
-
 	var full_path = scene_path + destination_scene + ".tscn"
 	get_tree().call_deferred("change_scene_to_file", full_path)
+
+	animation_player.play_backwards("transition_out")
+	await animation_player.animation_finished
+	if player:
+		player.can_move = true
